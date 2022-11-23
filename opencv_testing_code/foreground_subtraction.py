7fc467e2377a4b645813169549860808d1324c5d
Jerzy Ramos <gmjerzy@gmail.com>
2022-11-22 21:15:30 -0800

    Improve foreground subtraction quality

--- opencv_testing_code/foreground_subtraction.py
+++ opencv_testing_code/foreground_subtraction.py
@@ -14,22 +14,25 @@ def subsample(arr, size=16):
     row, col, depth = arr.shape
     n_row = (row - 1) // size + 1
     n_col = (col - 1) // size + 1
-    n = np.zeros((n_row, n_col,depth))
+    n = np.zeros((n_row, n_col, depth))
     for i in range(n_row):
         for j in range(n_col):
             for k in range(depth):
-                n[i][j][k] = arr[i * size:(i + 1) * size:, j * size:(j + 1) * size:,k].mean()
+                n[i][j][k] = arr[i * size:(i + 1) * size:, j * size:(j + 1) * size:, k].mean()
     return n
+
 
 # change args to use preset values for different videos
 
-# args = ["SAL_490_270_437", 1.1, 5, 0, 4]
-args = ["Stairs_490_270_346", 0.4, 5, 0, 4]
+# args = ["SAL_490_270_437", 1.1, 5, 0, 4, 2, 2]
+args = ["Stairs_490_270_346", 0.4, 15, 0, 8, 5, 0]
 foldername = args[0]
 ang_threshold = args[1]
 nearest_frames = args[2]
 quantile_threshold = args[3]
 min_shape_threshold = args[4]
+horizontal_edge_buffer = args[5]
+vertical_edge_buffer = args[6]
 
 print("Program started")
 
@@ -61,18 +64,19 @@ frame_masks = []
 # TODO - better feathering of shape?
 print("Frames processing started")
 for i in range(1, len(imgs)):
-    if i == len(imgs)//4:
+    if i == len(imgs) // 4:
         print("Processing 25% done")
-    elif i == len(imgs)//2:
+    elif i == len(imgs) // 2:
         print("Processing 50% done")
-    elif i == len(imgs)//4*3:
+    elif i == len(imgs) // 4 * 3:
         print("Processing 75% done")
-    next = cv.cvtColor(imgs[i], cv.COLOR_BGR2GRAY)
-    motion = cv.calcOpticalFlowFarneback(prvs, next, None, 0.5, 3, 16, 3, 5, 0.5, 1)
+    nxt = cv.cvtColor(imgs[i], cv.COLOR_BGR2GRAY)
+    motion = cv.calcOpticalFlowFarneback(prvs, nxt, None, 0.5, 3, 16, 3, 5, 0.5, 1)
     # mag, ang = cv.cartToPolar(motion[..., 0], motion[..., 1], angleInDegrees=True)
     size = 16
     subsampled_motion = subsample(motion, size)
-    subsampled_mag, subsampled_ang = cv.cartToPolar(subsampled_motion[..., 0], subsampled_motion[..., 1], angleInDegrees=True)
+    subsampled_mag, subsampled_ang = cv.cartToPolar(subsampled_motion[..., 0], subsampled_motion[..., 1],
+                                                    angleInDegrees=True)
     subsampled_ang = subsampled_ang / 72
     m, n = subsampled_ang.shape
     u, c = np.unique(subsampled_ang.round(), return_counts=True)
@@ -80,50 +84,30 @@ for i in range(1, len(imgs)):
     # u, c = np.unique(subsampled_mag.round(1), return_counts=True)
     # most_common_mag = u[c.argmax()]
 
-
     # diff_area = []
     mask = np.zeros_like(subsampled_ang)
-    
-    for x in range(m):
-        for y in range(n):
-            if min(abs(subsampled_ang[x][y] - most_common_ang), abs(5 - subsampled_ang[x][y] - most_common_ang)) > ang_threshold:
+
+    for x in range(vertical_edge_buffer, m-vertical_edge_buffer):
+        for y in range(horizontal_edge_buffer, n-horizontal_edge_buffer):
+            if min(abs(subsampled_ang[x][y] - most_common_ang),
+                   abs(5 - subsampled_ang[x][y] - most_common_ang)) > ang_threshold:
                 # TODO - Try to include magnitude difference into segmentation
                 # abs(subsampled_mag[x][y]-most_common_mag)>2:
                 # diff_area.append((x,y))
                 mask[x][y] = 1
-
     frame_masks.append(mask)
+    prvs = nxt
 
+video = cv.VideoWriter('foreground_video.mp4',cv.VideoWriter_fourcc(*'mp4v'), 20.0, (width, height))
 
-    prvs = next
-
-# cv.destroyAllWindows()
-# # check f neighbors both ways for each frame
-# f = 5
-# for i in range(f, len(foreground_frames)-f):
-#     # if most empty, leave empty
-#     # if few empty, ignore empty ones
-#     for shape in frame_shapes[i]:
-#         quantile_dict = defaultdict(int)
-#         px, py = shape[0]
-#         for frame in range(i-f,i+f):
-#             if frame == i:
-#                 continue
-#             cur_shape = shapes_dict[i][(px,py)]
-#             # got shape and cur_shape for comparison
-
-for i in range(len(imgs)):
-    matrix = np.mean(frame_masks[max(0,i-nearest_frames):i+nearest_frames], axis=0)
-    # for x in range(len(m)):
-    #     for y in range(len(m[0])):
-    #         if m[x][y] > 0:
-    #             ff[x * size:(x+1)*size, y*size:(y+1)*size] = imgs[i][x*size:(x+1)*size,y*size:(y+1)*size]
-    
-
+for i in range(len(imgs)-1):
+    matrix = np.mean(frame_masks[max(0, i - nearest_frames):i + nearest_frames], axis=0)
+    # frame_masks[i] = np.zeros(frame_masks[i].shape)
     # get contiguous shapes from diff_area
     total_shapes = 0
     shapes = {}
-    m,n = frame_masks[i].shape
+    m, n = frame_masks[i].shape
+
     for x in range(m):
         for y in range(n):
             if matrix[x][y] == 0:
@@ -137,6 +121,7 @@ for i in range(len(imgs)):
                 if 0 <= u < m and 0 <= v < n and (u, v) not in visited:
                     visited.add((u, v))
                     if matrix[u][v] > quantile_threshold:
+                        # frame_masks[i][u][v] = 1
                         area += 1
                         coord.add((u, v))
                         stack.append((u - 1, v))
@@ -157,24 +142,23 @@ for i in range(len(imgs)):
     for shape in shapes.values():
         if len(shape) > min_shape_threshold:
             for x, y in shape:
-                shapes_dict[(x,y)]=shape
-                foreground_frame[x * size:(x+1)*size, y*size:(y+1)*size] = imgs[i][x*size:(x+1)*size,y*size:(y+1)*size]
-                background_frame[x * size:(x+1)*size, y*size:(y+1)*size] = [0, 0, 0]
-
+                shapes_dict[(x, y)] = shape
+                foreground_frame[x * size:(x + 1) * size, y * size:(y + 1) * size] = imgs[i][x * size:(x + 1) * size,
+                                                                                     y * size:(y + 1) * size]
+                background_frame[x * size:(x + 1) * size, y * size:(y + 1) * size] = [0, 0, 0]
 
     # frame_shapes.append(shapes)
     # frame_shapes_dict.append(shapes_dict)
     foreground_frames.append(foreground_frame)
     background_frames.append(background_frame)
 
-
-
     cv.imshow('frame', foreground_frame)
     cv.setWindowProperty('frame', cv.WND_PROP_TOPMOST, 1)
     k = cv.waitKey(30) & 0xff
     if k == 27:
         break
-
+    video.write(foreground_frame)
 
 # new approach directly compare frames and take average
+video.release()
 cv.destroyAllWindows()
